1eb96f7138b0bfe5368b18c5e529d18c5fbac377
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-29 16:22:44 -0300

    Update QuickSortIterativo.java

--- QuickSortIterativo.java
+++ QuickSortIterativo.java
@@ -1,108 +1,94 @@
 public class QuickSortIterativo {
-    private static int trocas;
-    private static int iteracoes;
-
     public static void main(String[] args) {
-        // Configurar datasets
-        int[][] datasets = {
-                {1, 100, 2, 99, 3, 98, 4, 97, 5, 96, 6, 95, 7, 94, 8, 93, 9, 92, 10, 91, 11, 90, 12, 89, 13, 88, 14, 87, 15, 86, 16, 85, 17, 84, 18, 83, 19, 82, 20, 81, 21, 80, 22, 79, 23, 78, 24, 77, 25, 76},
-                {1, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52},
-                {50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-        };
 
-        // Armazenamento das métricas
-        int[] temposExecucao = new int[3];
-        int[] trocasTotais = new int[3];
-        int[] iteracoesTotais = new int[3];
+        int[] tempos = new int[3];
+        int[] trocas = new int[3];
+        int[] iteracoes = new int[3];
 
-        // Processar cada dataset
-        for (int i = 0; i < datasets.length; i++) {
-            trocas = 0;
-            iteracoes = 0;
 
-            int[] array = datasets[i].clone(); // Evitar modificar o dataset original
-            long inicio = System.nanoTime(); // Para medir o tempo de execução
-            quickSort(array);
-            long fim = System.nanoTime(); // Para medir o tempo de execução
+        int[] dataset1 = {1, 100, 2, 99, 3, 98, 4, 97, 5, 96, 6, 95, 7, 94, 8, 93, 9, 92, 10, 91, 11, 90, 12, 89, 13, 88, 14, 87, 15, 86, 16, 85, 17, 84, 18, 83, 19, 82, 20, 81, 21, 80, 22, 79, 23, 78, 24, 77, 25, 76};
+        int[] dataset2 = {1, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52};
+        int[] dataset3 = {50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-            // Salvar as métricas nos arrays
-            temposExecucao[i] = (int) (fim - inicio);
-            trocasTotais[i] = trocas;
-            iteracoesTotais[i] = iteracoes;
 
-            System.out.println("Dataset " + (i + 1) + " ordenado:");
-            imprimirArray(array);
-        }
+        int tamanhoDataset1 = 50;
+        int tamanhoDataset2 = 50;
+        int tamanhoDataset3 = 50;
 
-        // Exibir métricas
-        for (int i = 0; i < datasets.length; i++) {
+
+        processarQuickSort(dataset1, tamanhoDataset1, tempos, trocas, iteracoes, 0);
+        processarQuickSort(dataset2, tamanhoDataset2, tempos, trocas, iteracoes, 1);
+        processarQuickSort(dataset3, tamanhoDataset3, tempos, trocas, iteracoes, 2);
+
+
+        for (int i = 0; i < 3; i++) {
             System.out.println("Dataset " + (i + 1) + ":");
-            System.out.println("Tempo de execução (ns): " + temposExecucao[i]);
-            System.out.println("Número de trocas: " + trocasTotais[i]);
-            System.out.println("Número de iterações: " + iteracoesTotais[i]);
+            System.out.println("Tempo de execução (ns): " + tempos[i]);
+            System.out.println("Número de trocas: " + trocas[i]);
+            System.out.println("Número de iterações: " + iteracoes[i]);
             System.out.println();
         }
     }
 
-    // Implementação do QuickSort Iterativo
-    public static void quickSort(int[] array) {
-        int tamanho = array.length;
-        int[] stack = new int[tamanho];
-        int topo = -1;
+    public static void processarQuickSort(int[] array, int tamanho, int[] tempos, int[] trocas, int[] iteracoes, int index) {
+        long startTime = System.nanoTime();
+        int[] metrics = quickSort(array, tamanho);
+        long endTime = System.nanoTime();
 
-        // Empilhar os limites iniciais do array
-        stack[++topo] = 0;
-        stack[++topo] = tamanho - 1;
-
-        // Enquanto houver elementos para particionar
-        while (topo >= 0) {
-            iteracoes++;
-            int end = stack[topo--];
-            int start = stack[topo--];
-
-            int pivoIndex = particionar(array, start, end);
-
-            // Empilhar as novas partições
-            if (pivoIndex - 1 > start) {
-                stack[++topo] = start;
-                stack[++topo] = pivoIndex - 1;
-            }
-            if (pivoIndex + 1 < end) {
-                stack[++topo] = pivoIndex + 1;
-                stack[++topo] = end;
-            }
-        }
+        long duration = endTime - startTime;
+        tempos[index] = (int) duration;
+        trocas[index] = metrics[0];
+        iteracoes[index] = metrics[1];
     }
 
-    // Função de particionamento
-    public static int particionar(int[] array, int start, int end) {
-        int pivo = array[end];
-        int i = start - 1;
+    public static int[] quickSort(int[] array, int tamanho) {
+        int[] stack = new int[tamanho * 2];
+        int top = -1;
+        int numTrocas = 0;
+        int numIteracoes = 0;
 
-        for (int j = start; j < end; j++) {
-            iteracoes++;
-            if (array[j] <= pivo) {
+        stack[++top] = 0;
+        stack[++top] = tamanho - 1;
+
+        while (top >= 0) {
+            int high = stack[top--];
+            int low = stack[top--];
+
+            int p = partition(array, low, high);
+            numIteracoes++;
+
+            if (p - 1 > low) {
+                stack[++top] = low;
+                stack[++top] = p - 1;
+            }
+
+            if (p + 1 < high) {
+                stack[++top] = p + 1;
+                stack[++top] = high;
+            }
+
+            numTrocas++;
+        }
+
+        return new int[]{numTrocas, numIteracoes};
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
                 i++;
-                trocar(array, i, j);
+                swap(array, i, j);
             }
         }
-        trocar(array, i + 1, end);
+        swap(array, i + 1, high);
         return i + 1;
     }
 
-    // Função para trocar os elementos
-    public static void trocar(int[] array, int i, int j) {
-        trocas++;
+    public static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-    }
-
-    // Função para imprimir o array
-    public static void imprimirArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
     }
 }
